8da05ed2b379956a95ae8bbf0df26f179b12f0f8
pyro <virusgame97@gmail.com>
2020-04-01 00:32:05 +0200

    Lanciafiamme

--- Assets/PRODUZIONE/Script/Persona.cs
+++ Assets/PRODUZIONE/Script/Persona.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Persona : MonoBehaviour
 {
     // Start is called before the first frame update
+    private EnemiesGenerator enemies;
+
     void Start()
     {
-        
+        enemies = GameObject.FindObjectOfType<EnemiesGenerator>();
+
     }
 
     // Update is called once per frame
@@ -16,20 +19,16 @@ public class Persona : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnParticleCollision(GameObject other)
     {
-        if (collision.gameObject.tag.Equals("Fire"))
+        if (other.gameObject.tag.Equals("Fire"))
         {
-            Destroy(collision.gameObject);
-
+            Destroy(this.gameObject);
+            enemies.enemies.Remove(this.gameObject);
         }
     }
 
-    private void OnParticleCollision(GameObject other)
-    {
-        print("ok");
-            Destroy(this.gameObject);
+    
 
-        
-    }
+
 }
