2301b1a0c93f4717e734c24289d190a2966bc211
pyro <virusgame97@gmail.com>
2020-04-09 18:59:04 +0200

    User

---
+++ Assets/PRODUZIONE/Script/ListaUsers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+
+public class ListaUsers
+{
+    public List<User> utenti=new List<User>();
+
+    public ListaUsers(List<User> utenti)
+    {
+        this.utenti = utenti;
+    }
+
+  
+}
---
+++ Assets/PRODUZIONE/Script/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class User
+{
+
+    public string userName="";
+    public int userPunteggio=0;
+
+    public User(string userName,int userPunteggio)
+    {
+        this.userName = userName;
+        this.userPunteggio = userPunteggio;
+    }
+
+    public User()
+    {
+
+    }
+
+}
